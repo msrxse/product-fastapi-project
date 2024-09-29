8d6304df53db8a3810bb38960e9b1e30d9745b67
msr <msrxse@gmail.com>
2024-09-29 21:55:44 +0100

    Add attribute table and tests

--- app/models.py
+++ app/models.py
@@ -155,3 +155,19 @@ class SeasonalEvents(Base):
         ),
         UniqueConstraint("name", name="uq_seasonal_event_name"),
     )
+
+
+class Attribute(Base):
+    __tablename__ = "attribute"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(100), nullable=False)
+    description = Column(String(100), nullable=True)
+
+    __table_args__ = (
+        CheckConstraint(
+            "LENGTH(name) > 0",
+            name="attribute_name_length_check",
+        ),
+        UniqueConstraint("name", name="uq_attribute_name"),
+    )
